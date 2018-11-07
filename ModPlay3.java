9e1a54df4ece5fbc490ac00da6e4bdb854c589ab
martincameron <mumart@gmail.com>
2018-11-07 22:25:49 +0000

    Initial sequencer.

--- ModPlay3.java
+++ ModPlay3.java
@@ -6,11 +6,14 @@ public class ModPlay3
 	private static final int FIXED_POINT_SHIFT = 13;
 	private static final int FIXED_POINT_ONE = 1 << FIXED_POINT_SHIFT;
 	
+	private int sampleRate;
+	
 	private String songName;
 	private String[] instrumentNames = new String[ NUM_SAMPLES ];
 	private int numChannels;
 	private int songLength;
 	private int numPatterns;
+	private int c2Rate;
 	private byte[] sequence;
 	private byte[] patternData;
 	private byte[][] sampleData = new byte[ NUM_SAMPLES ][];
@@ -25,18 +28,26 @@ public class ModPlay3
 	private int[] channelSamplePos = new int[ MAX_CHANNELS ];
 	private int[] channelStep = new int[ MAX_CHANNELS ];
 	
-	private int tickPosition;
-	private int tickLength;
+	private int tickSamplePos;
+	private int tickSampleLen;
 	
-	public ModPlay3( java.io.InputStream moduleData ) throws java.io.IOException
+	private int currentSequencePos;
+	private int nextSequencePos;
+	private int currentRow;
+	private int nextRow;
+	private int currentTick;
+	private int ticksPerRow = 6;
+	private int tempo = 125;
+	
+	public ModPlay3( int sampleRate, java.io.InputStream moduleData ) throws java.io.IOException
 	{
-		final String ISO_LATIN_1 = "ISO-8859-1";
-		songName = new String( readBytes( moduleData, 20 ), ISO_LATIN_1 ).trim();
+		this.sampleRate = sampleRate;
+		songName = readString( moduleData, 20 ).trim();
 System.out.println(songName);
 		int[] sampleLengths = new int[ NUM_SAMPLES ];
 		for( int idx = 0; idx < NUM_SAMPLES; idx++ )
 		{
-			instrumentNames[ idx ] = new String( readBytes( moduleData, 22 ), ISO_LATIN_1 ).trim();
+			instrumentNames[ idx ] = readString( moduleData, 22 ).trim();
 			sampleLengths[ idx ] = ( ( ( moduleData.read() & 0xFF ) << 8 ) | ( moduleData.read() & 0xFF ) ) * 2;
 			sampleFineTune[ idx ] = ( byte ) ( moduleData.read() & 0xF );
 			sampleVolume[ idx ] = ( byte ) ( moduleData.read() & 0x7F );
@@ -64,10 +75,11 @@ if(instrumentNames[idx].length() > 0 )System.out.println(instrumentNames[idx]);
 				numPatterns = sequence[ idx ] + 1;
 			}
 		}
-		String modType = new String( readBytes( moduleData, 4 ), ISO_LATIN_1 );
+		String modType = readString( moduleData, 4 );
 		if( modType.equals( "M.K." ) )
 		{
 			numChannels = 4;
+			c2Rate = 8287;
 		}
 		else
 		{
@@ -82,6 +94,12 @@ if(instrumentNames[idx].length() > 0 )System.out.println(instrumentNames[idx]);
 	
 	public void getAudio( int[] output, int count )
 	{
+		if( tickSamplePos++ >= tickSampleLen )
+		{
+			tickSamplePos = 0;
+			tickSampleLen = ( sampleRate * 5 ) / ( tempo * 2 );
+			tick();
+		}
 		for( int idx = 0, end = count * 2; idx < end; idx += 2 )
 		{
 			int lamp = 0, ramp = 0;
@@ -109,21 +127,36 @@ if(instrumentNames[idx].length() > 0 )System.out.println(instrumentNames[idx]);
 			}
 			output[ idx ] = lamp;
 			output[ idx + 1 ] = ramp;
-			tickPosition++;
-			if( tickPosition > tickLength )
-			{
-				tickPosition = 0;
-				tick();
-			}
 		}
 	}
 	
 	private void row()
 	{
+		currentSequencePos = nextSequencePos;
+		currentRow = nextRow;
+		currentTick = ticksPerRow;
+		nextRow = currentRow + 1;
+		if( nextRow > 63 )
+		{
+			nextRow = 0;
+			nextSequencePos = currentSequencePos + 1;
+			if( nextSequencePos >= songLength )
+			{
+				nextSequencePos = 0;
+			}
+		}
 	}
 	
 	private void tick()
 	{
+		if( currentTick-- <= 0 )
+		{
+			row();
+		}
+		for( int chn = 0; chn < numChannels; chn++ ) 
+		{
+			channelStep[ chn ] = ( c2Rate * 428 / channelPeriod[ chn ] ) * FIXED_POINT_ONE / sampleRate;
+		}
 	}
 	
 	private static byte[] readBytes( java.io.InputStream inputStream, int length ) throws java.io.IOException
@@ -139,11 +172,16 @@ if(instrumentNames[idx].length() > 0 )System.out.println(instrumentNames[idx]);
 		return bytes;
 	}
 	
+	private static String readString( java.io.InputStream inputStream, int length ) throws java.io.IOException
+	{
+		return new String( readBytes( inputStream, length ), "ISO-8859-1" );
+	}
+	
 	public static void main( String[] args ) throws java.io.IOException {
 		java.io.InputStream inputStream = new java.io.FileInputStream( args[ 0 ] );
 		try
 		{
-			ModPlay3 modPlay3 = new ModPlay3( inputStream );
+			ModPlay3 modPlay3 = new ModPlay3( 48000, inputStream );
 		}
 		finally
 		{
